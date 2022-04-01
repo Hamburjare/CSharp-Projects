63d41794306a18cd0ca84d62751e1745536b0dee
Hamburjare <93974508+Hamburjare@users.noreply.github.com>
2022-04-01 14:02:16 +0300

    Taulukoiden monistus

--- Taulukoiden monistus/Program.cs
+++ Taulukoiden monistus/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Taulukoiden_monistus
+int[] array = new int[5];
+int[] array2 = new int[5];
+
+for (int i = 0; i < array.Length; i++)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.Write($"Anna numero listan {i + 1} kohtaan: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    array[i] = number;
 }
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+for (int i = 0; i < array.Length; i++)
+{
+    int number = array[i];
+    array2[i] = number;
+}
+Console.WriteLine("[{0}]", string.Join(", ", array2));
