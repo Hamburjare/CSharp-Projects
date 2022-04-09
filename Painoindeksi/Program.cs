c5fffa72be1c6eb26f651b5f15b9b82c3fdbbac8
Hamburjare <93974508+Hamburjare@users.noreply.github.com>
2022-04-10 00:03:25 +0300

    painoindeksi laskuri

---
+++ Painoindeksi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Write("Anna pituus (cm): ");
+double pituus = Convert.ToInt32(Console.ReadLine());
+pituus = (pituus * 1.0 / 100);
+
+Console.Write("Anna paino (kg): ");
+int paino = Convert.ToInt32(Console.ReadLine());
+
+double value = Math.Pow(pituus, 2.5);
+
+double lasku = 1.3 * paino / value;
+
+lasku = Math.Round(lasku, 2);
+
+Console.WriteLine($"Paino indeksisi on {lasku} ");
